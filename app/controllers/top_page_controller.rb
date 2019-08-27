6444d0be02a45672dd2b8bfdd20ba5c12ab6da42
shiro <shiro.mns@gmail.com>
2019-08-27 11:10:52 +0900

    [追加]トップページ更新日時が表示されるように

--- app/controllers/top_page_controller.rb
+++ app/controllers/top_page_controller.rb
@@ -1,6 +1,6 @@
 class TopPageController < ApplicationController
   def index
-    #@latest_created = Status.maximum("created_at")
+    @latest_created = Status.maximum("created_at")
     @uploaded_created = UploadedCheck.maximum("created_at")
   end
 end
